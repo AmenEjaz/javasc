51bd6f882efdaf8ad83ba506a4f96870f4e064f4
Amen Ejaz <swatipk111@gmail.com>
2026-07-10 11:25:46 +0000

    Datatype summary

---
+++ basics/conversionOperation.js
@@ -0,0 +1,41 @@
+//Number to String
+let number=23
+let NumberToString=String(number)
+// console.log("Number to String",NumberToString)
+// console.log(typeof NumberToString)
+
+//String to Number
+let number2="23"
+let StringToNumber=Number(number)
+// console.log("String to Number",StringToNumber)
+// console.log(typeof StringToNumber)
+
+let negNumber=-number
+/*console.log("Negative of ",number ,"=", negNumber)
+console.table([number+number, number-number, number*number, number/number, number%number, number%0, 0%number])
+*/
+let str1="Hello"
+let str2="Newton"
+/*
+console.log(str1+str2)
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+ console.log(+true)
+ console.log(+ "")
+ */
+
+let num1, num2, num3
+//num1=num2=num3=4
+num1=num2=num3 =4+6
+console.log(num1,num2,num3)
+let num=45
+
+console.log(num,num++, ++num)  //45 45 47
+console.log(--num)             // 44
+console.log(num--)             //45
+
+console.log(num,num--, --num)  //45 45 43
---
+++ basics/datatype_summary.js
@@ -0,0 +1,30 @@
+//Primitive
+// const score=100
+// const value=92.8
+// const isLoggedIn =false
+ const Temp=null
+// let userEmail
+ const id= Symbol('12')
+// const id2=Symbol(12)
+// console.log(id==id2);
+ bigInt=432178921376123n
+
+// //Non-premitive (Reference)
+// const students=["Amen", Kanwal];
+let obj={
+    name:"Amen",
+    age:23,
+}
+// const myFunction =function(){
+//     console.log("Hello");
+// }
+// console.log(typeof bigInt)
+// console.log(typeof null)
+// console.log(typeof myFunction)
+// console.log(typeof id)
+console.log(typeof obj)
+
+
+
+
+
