a57f4b8298684ca55545ce73dd57a07ec143a10f
Amen Ejaz <swatipk111@gmail.com>
2026-07-23 12:21:46 +0000

    JS

---
+++ basics/interval.js
@@ -0,0 +1,11 @@
+  const changeText=function(){
+        document.querySelector('#heading').innerHTML='Java Script'
+    }
+   // addEventListener('click',changeText)
+   
+    const event= setTimeout(changeText,3000)
+      //Selecting the button reverse the change
+   document.querySelector('#stop').addEventListener('click', function(){
+    console.log("change reversed")
+    document.querySelector('#heading').innerHTML="Web Development"
+   })
